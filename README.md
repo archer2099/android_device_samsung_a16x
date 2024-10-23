5a609dcd650a3f27a7b19d378ed787a15924c01b
Archer2099 <nzero0973@gmail.com>
2024-10-24 00:33:48 +0700

    a16x Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TWRP device tree for samsung Galaxy A16 5G (Exynos)
+
+## Credit:
+[Gabriel](https://github.com/Gabriel2392) For some stuff of a14x.
+
+[Hunter](https://github.com/devhunter1) For helping me.
+
+## How-to compile it:
+
+```sh
+. build/envsetup.sh
+lunch twrp_a16x-eng
+make recoveryimage
+```
---
+++ twrp_a16x.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := b6q
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from device
+$(call inherit-product, device/samsung/a16x/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a16x/recovery/root,recovery/root)
+
+PRODUCT_DEVICE := a16x
+PRODUCT_NAME := twrp_a16x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A166B
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
